b3b2f1f2e2089b0ec773f7dd86bcf49b9805e3c0
fadywadea <fwadea3@gmail.com>
2024-04-22 23:55:53 +0200

    add: wishlist file

--- src/modules/wishlist/wishlist.controller.js
+++ src/modules/wishlist/wishlist.controller.js
@@ -2,10 +2,12 @@
 
 import { userModel } from "../../../database/models/user.model.js";
 import { catchError } from "../../middleware/catchError.js";
+import { AppError } from "../../utils/appError.js";
 
 const updateWishlist = catchError(async (req, res, next) => {
-  let wishlist = await userModel.findByIdAndUpdate(req.params.id, req.body, { new: true, });
-  !wishlist && next(new AppError("wishlist not found.", 404));
+  let wishlist = await userModel.
+    findByIdAndUpdate(req.user._id, { $addToSet: { wishlist: req.body.product } }, { new: true, });
+  !wishlist && next(new AppError("The wish list is empty.", 404));
   wishlist && res.status(200).json({ message: "success", wishlist });
 });
 
