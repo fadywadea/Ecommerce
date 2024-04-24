f685641af9e221837a9c9ae57ef58ba3b8fb3302
fadywadea <fwadea3@gmail.com>
2024-04-24 22:32:09 +0200

    Edit: coupon.model

--- database/models/coupon.model.js
+++ database/models/coupon.model.js
@@ -5,14 +5,13 @@ import mongoose from "mongoose";
 const couponSchema = new mongoose.Schema({
   code: {
     type: String,
+    unique: [true, "name is required"],
     trim: true,
-    required: true
+    required: true,
+    minLength: [1, "too short coupon code"]
   },
+  discount: Number,
   expires: Date,
-  discount: {
-    type: Number,
-    required: true
-  },
   createdBy: {
     type: mongoose.Types.ObjectId,
     ref: "user",
