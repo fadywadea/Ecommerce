4a47ed302cdac9369276faf6084b4e73895cc1dd
fadywadea <fwadea3@gmail.com>
2024-04-25 19:59:00 +0200

    create cart model

---
+++ database/models/cart.model.js
@@ -0,0 +1,19 @@
+'use strict';
+
+import mongoose from "mongoose";
+
+const cartSchema = new mongoose.Schema({
+  user: { type: mongoose.Types.ObjectId, ref: 'user' },
+  cartItems: [{
+    product: { type: mongoose.Types.ObjectId, ref: 'product' },
+    quantity: Number,
+    price: Number
+  }],
+  totalPrice: Number,
+  totalPriceAfterDiscount: Number,
+  discount: Number
+},
+  { timestamps: true }
+);
+
+export const cartModel = mongoose.model("cart", cartSchema);
