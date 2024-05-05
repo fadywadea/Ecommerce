eb49bfbe0cd49925803b8b3626e1574bc5ef707e
fadywadea <fwadea3@gmail.com>
2024-05-05 09:21:37 +0300

    add: authorization and authentication

--- src/modules/product/product.routes.js
+++ src/modules/product/product.routes.js
@@ -6,20 +6,21 @@ import { uploadFields } from "../../services/fileUploads/fileUpload.js";
 import { addProduct, deleteProduct, getAllProducts, getSingleProduct, updateProduct } from "./product.controller.js";
 import { addProductVal, paramsIdVal, updateProductVal } from "./product.validation.js";
 import { conditions } from "../../middleware/ifConditions.js";
+import { authorization, protectedRoutes } from "../auth/auth.controller.js";
 
 const productRouter = express.Router();
 
 productRouter
   .route("/")
-  .post(uploadFields([{ name: 'imgCover', maxCount: 1 }, { name: 'images', maxCount: 10 }]),
+  .post(protectedRoutes, authorization("admin"), uploadFields([{ name: 'imgCover', maxCount: 1 }, { name: 'images', maxCount: 10 }]),
     validation(addProductVal), conditions, addProduct)
   .get(getAllProducts);
 
 productRouter
   .route("/:id")
   .get(validation(paramsIdVal), getSingleProduct)
-  .put(uploadFields([{ name: 'imgCover', maxCount: 1 }, { name: 'images', maxCount: 10 }]),
+  .put(protectedRoutes, authorization("admin"), uploadFields([{ name: 'imgCover', maxCount: 1 }, { name: 'images', maxCount: 10 }]),
     validation(updateProductVal), conditions, updateProduct)
-  .delete(validation(paramsIdVal), deleteProduct);
+  .delete(protectedRoutes, authorization("admin"), validation(paramsIdVal), deleteProduct);
 
 export default productRouter;
